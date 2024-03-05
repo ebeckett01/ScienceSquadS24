e5eb079144e069956888778b6bd8ad7a46d255a2
ebeckett01 <ebeckett@mines.edu>
2024-03-05 10:23:26 -0700

    Added Demos

---
+++ recieve_demo/recieve_demo.ino
@@ -0,0 +1,48 @@
+//same libraries and object creation as your transmitter
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(9, 8);  // CE, CSN
+
+//Yes you have to have same structure variable 'DataPacket' 
+struct DataPacket {
+
+  //THIS IS YOUR TRANSMISSION VARIABALE INITIALIZATION BLOCK
+  
+  //Yes, this is and must be the same block from your transmitter code
+  //Again, yes, you must have the same transmission variables...
+  //  In the reciever code as you do in the transmitter code
+  char StrHey;//character variable
+
+  //THIS IS YOUR TRANSMISSION VARIABALE INITIALIZATION BLOCK
+
+};
+
+//check transmit_demo if confused - all of this is the same
+DataPacket data; //same structure variables 'DataPacket and 'data' as transmitter
+
+//address through which two modules communicate.
+const byte address[6] = "00001";
+
+
+void setup() {
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.openReadingPipe(0, address);//set receiving to 5-digit address
+  radio.startListening();//pretty explicit code call
+  //"Sometimes I wish my students had this built-in function" - Prof. Kuska circa 2024
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (radio.available()) {//if a signal is available
+    radio.read(&data, sizeof(DataPacket));//read signal being sent
+    //the data structure is now here - weeeeeeeeeeeee
+
+    //we can now use variables in this void loop(). SICK!
+    Serial.println(data.StrHey);//print that variable we created and transmitted
+    //what do you think it will print?!
+
+  }
+}
---
+++ transmit_demo/transmit_demo.ino
@@ -0,0 +1,72 @@
+//include the necessary libraries for the RF24 Tx/Rx
+//Tx = transmitter
+//Rx = reciever
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//create the RF24 object
+RF24 radio(9, 8);  // CE, CSN
+//this object is recognized by the RF24 library
+//and takes the two pin arguments for the CE and CSN pin
+//CE is basically the input pin and is used to control data transmission
+//CSN pin is a chip selection pin and turns on and off to recieve or transmit signal
+//if you followed the provided wiring diagram then you do not need to change these
+
+//a structure is a variable that can hold more variables
+//think of it like a filing cabnet with as many files in it as you want
+//where each file is a different variable packaged into the larger structure DataPacket
+struct DataPacket {
+
+  //THIS IS YOUR TRANSMISSION VARIABALE INITIALIZATION BLOCK
+
+  //THIS is where you will initialize all variables for the control of the CoLeBot
+  //You MUST initialize a variable corresponding to each sensor value you decide to use
+  //Examples inlcude: a joystick's x and y value and its button, or another button or sensor of your choosing
+  //Here to show an example I am creating a character - a variable that can hold a single letter
+  char StrHey;//character variable
+
+  //THIS IS YOUR TRANSMISSION VARIABALE INITIALIZATION BLOCK
+
+};
+
+//assign a variable to hold all of the desired data to be transmitted
+//in this case our variable is called 'data'
+DataPacket data;
+
+//create a unique 'address' for your tx and rx
+//this needs to be unique and the same between both your tx and rx
+//if there are many with the same number there will be cross-talk
+//if they aren't matching... guess what happens
+//transmitted data is identified by this 5-digit pin, so rx's will only look for data with this identifier
+const byte address[6] = "00001";
+//you can change the 5-digit number but do not change the variable initialization or name
+//those are necessary for the RF24 library
+
+void setup() {
+  radio.begin();//Tell the RF library to start radio communication
+  radio.setPALevel(RF24_PA_LOW);
+  radio.openWritingPipe(address);//set transmission address to 5-digit number above
+  //Set module as transmitter
+  radio.stopListening();
+  Serial.begin(9600);//you better know what this does
+}
+
+void loop() {
+
+  //THIS IS YOUR VARIABALE ALLOCATION BLOCK
+
+  //This is where you will allocate values to the initialized variables in your DataPacket STRUCTure
+  //YOU WILL NEED TO DO THIS FOR EACH SENSOR READING
+  //TL;DR - read each sensor and save its readings to an initialized variables above
+  data.StrHey = 'a';//assign your favorite letter - characters are in single quotes
+  //saving that character variable into our data variable which is in our DataPacket structure
+
+  //THIS IS the end of YOUR VARIABALE ALLOCATION BLOCK
+
+  Serial.println(data.StrHey);//print the variable were transmitting here
+  //not necessary but just for funsies
+  radio.write(&data, sizeof(DataPacket));//transmit data
+  //here we are transmitting the structure 'data' within the strucure 'DataPacket'
+  delay(20);//chill out for like 20 ms...
+}
